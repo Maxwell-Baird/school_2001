99d037cf1ced05f041cc940ad5ed89d10edd2c63
Maxwell Baird <bairdm71@gmail.com>
2020-01-29 13:09:12 -0700

    Initialize school

---
+++ lib/school.rb
@@ -0,0 +1,8 @@
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+  def initialize(time, hours)
+    @start_time = time
+    @hours_in_school_day = hours
+    @student_names = []
+  end
+end
